07cd911c67c59c1b69e2e8c9123cae8dc349ebd5
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2014-06-01 19:21:58 +0100

    Results displayed in a table

--- RWTFlickrSearch/View/RWTSearchResultsViewController.m
+++ RWTFlickrSearch/View/RWTSearchResultsViewController.m
@@ -6,7 +6,7 @@
 #import "RWTSearchResultsViewController.h"
 #import <ReactiveCocoa/ReactiveCocoa.h>
 
-@interface RWTSearchResultsViewController ()
+@interface RWTSearchResultsViewController () <UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITableView *searchResultsTable;
 
@@ -21,6 +21,29 @@
     _viewModel = viewModel;
   }
   return self;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  [self.searchResultsTable registerClass:UITableViewCell.class forCellReuseIdentifier:@"cell"];
+  self.searchResultsTable.dataSource = self;
+  
+  [self bindViewModel];
+}
+
+- (void)bindViewModel {
+  self.title = self.viewModel.title;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return self.viewModel.searchResults.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+  cell.textLabel.text = [self.viewModel.searchResults[indexPath.row] title];
+  return cell;
 }
 
 @end
